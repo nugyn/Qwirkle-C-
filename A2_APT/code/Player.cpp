a516abcd335a854c4a21f60828dd90e7eaa72c8c
Justin Cooper <s3720189@student.rmit.edu.au>
2019-05-14 09:48:06 +1000

    Edited method return types

--- A2_APT/code/Player.cpp
+++ A2_APT/code/Player.cpp
@@ -1,6 +1,64 @@
-#include "Node.h";
+#include "Player.h";
 
+Player::Player(std::string name){
+  this -> name = name;
+}
 
+Player::Player(std::string name, LinkedList hand){
+  this -> name = name;
+  this -> hand = hand;
+}
 
+Player::~Player(){
 
+}
 
+Player::setHand(LinkedList hand){
+  this -> hand = hand;
+}
+
+Player::setPoints(int points){
+  this -> points = points;
+}
+
+Player::addPoints(int points){
+  this -> points += points;
+}
+
+Player::getHand(){
+  LinkedList* handPtr = &hand;
+  return handPtr;
+}
+
+Player::getPoints(){
+  int* pointsPtr = &points;
+  return pointsPtr;
+}
+
+Player::getName(){
+  std::string* namePtr = &name;
+  return namePtr;
+}
+
+Player::addTile(Tile tile){
+  hand.insertBack(tile);
+}
+
+Player::removeTile(Colour colour, Shape shape){
+  return findTile(colour, shape);
+}
+
+// Moved out in case it would be needed for any other method, if not
+// could move back into removeTile.
+Player::findTile(Colour colour, Shape shape){
+  // Don't like exit condition for this loop, will discuss a better solution
+  Tile* foundTile = nullptr;
+  for(int i = 1; i < hand.size(); ++i){
+    Tile* tempTile = hand.getTile(i);
+    if(tempTile.colour == colour && tempTile.shape == shape){
+      foundTile = tempTile;
+      i = hand.size();
+    }
+  }
+  return foundTile;
+}
--- A2_APT/code/Player.h
+++ A2_APT/code/Player.h
@@ -1,8 +1,39 @@
-
-
+#include "LinkedList.h"
 
 class Player{
 
 public:
+    std::string name;
+    int points;
+    LinkedList hand;
 
+    Player(std::string name);
+    Player(std::string name, LinkedList hand);
+    ~Player();
+
+    // Testing method, sets hand to given LinkedList
+    void setHand(LinkedList hand);
+    // Sets player score to given value
+    void setPoints(int points);
+    // Increases player score by given value
+    void addPoints(int points);
+
+    // Returns pointer to hand
+    LinkedList* getHand();
+
+    // Returns pointer to points
+    int* getPoints();
+
+    // Returns pointer to name
+    std::string* getName();
+
+    // Adds tile to hand
+    void addTile(Tile tile);
+    // Gets tile from hand
+    Tile* getTile(Colour color, Shape shape);
+    // Removes tile from hand
+    void removeTile(Tile* tile);
+
+  private:
+    Tile* findTile(Tile tile);
 }
