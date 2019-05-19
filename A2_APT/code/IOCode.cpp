2fc7a143df9de151578bdda1c90f1a6b03c2c800
nugyn <linhduyn@gmail.com>
2019-05-19 17:08:42 +1000

    IOCode changes
    Added header file, cleaned up IOCode file. Pending load/save functionality testing

--- A2_APT/code/IOCode.cpp
+++ A2_APT/code/IOCode.cpp
@@ -1,29 +1,10 @@
-#include <iostream>
-#include <fstream>
-#include <string>
-#include <sstream>
 
-using namespace std;
-
-struct SaveGame
-{
-    string playerName1;
-    int playerScore1;
-    string playerHand1;
-
-    string playerName2;
-    int playerScore2;
-    string playerHand2;
-
-    string board;
-    string bag;
-    string currentPlayer;
-};
+#include "IOCode.h"
 
 void writeFile(SaveGame s)
 {
 
-    ofstream outputFile;
+    std::ofstream outputFile;
     outputFile.open("savegame.txt");
 
     outputFile << s.playerName1 << endl;
@@ -41,31 +22,31 @@ void writeFile(SaveGame s)
     outputFile.close();
 }
 
-SaveGame readFile(string fileName)
+SaveGame readFile(std::string fileName)
 {
 
-    string line;
-    ifstream inFile;
+    std::string line;
+    std::ifstream inFile;
     inFile.open("savegame.txt");
 
-    struct saveGame
+    struct SaveGame
     {
-        string playerName1;
+        std::string playerName1;
         int playerScore1;
-        string playerHand1;
+        std::string playerHand1;
 
-        string playerName2;
+        std::string playerName2;
         int playerScore2;
-        string playerHand2;
+        std::string playerHand2;
 
-        string board;
-        string bag;
-        string currentPlayer;
+        std::string board;
+        std::string bag;
+        std::string currentPlayer;
     };
 
     SaveGame sg;
-    string value = "";
-    string board = "";
+    std::string value = "";
+    std::string board = "";
     int playerPos = 1;
     int currentPlayer = 1;
     int boardPos = 9;
@@ -75,7 +56,7 @@ SaveGame readFile(string fileName)
     {
         if ((playerPos % 3) == 1) //read first line
         {
-            string value = line;
+            std::string value = line;
             if (currentPlayer == 1)
             {
                 sg.playerName1 = value;
@@ -114,7 +95,7 @@ SaveGame readFile(string fileName)
         }
         else if ((playerPos % 3) == 0) //read third line
         {
-            string value = line;
+            std::string value = line;
 
             if (currentPlayer == 1)
             {
@@ -130,24 +111,22 @@ SaveGame readFile(string fileName)
                 value = "It's done";
             }
         }
-        //std::cout << value << "|" <<currentPlayer << std::endl;
-        if (currentPlayer > 2 && boardFill == true)
+        if (currentPlayer > 2 && boardFill == true) // allPlayers read, now read the board
         {
-                
-            if(boardPos == 9){
+
+            if (boardPos == 9)
+            {
                 --boardPos;
             }
             else if (boardPos == 8)
             {
                 sg.board += line + "\n";
 
-
                 --boardPos;
             }
             else if (boardPos == 7)
             {
                 sg.board += line + "\n";
-
 
                 --boardPos;
             }
@@ -198,43 +177,12 @@ SaveGame readFile(string fileName)
         }
         boardFill = true;
     }
-    // std::cout << sg.playerName1 << std::endl;
-    // std::cout << sg.playerScore1 << std::endl;
-    // std::cout << sg.playerHand1 << std::endl;
-    // std::cout << sg.playerName2 << std::endl;
-    // std::cout << sg.playerScore2 << std::endl;
-    // std::cout << sg.playerHand2 << std::endl;
 
     std::cout << sg.board << std::endl;
 
     std::cout << sg.bag << std::endl;
 
-    std::cout << sg.currentPlayer<< std::endl;
+    std::cout << sg.currentPlayer << std::endl;
 
     return sg;
-}
-
-int main()
-{
-    struct SaveGame saveGame;
-    saveGame.playerName1 = "Duy";
-    saveGame.playerScore1 = 0;
-    saveGame.playerHand1 = "R1,R2,R3,R4,R5";
-
-    saveGame.playerName2 = "Duy Linh";
-    saveGame.playerScore2 = 0;
-    saveGame.playerHand2 = "R1,R2,R3,R4,R5";
-
-    saveGame.board = "0  1  2  3  4  5  6\n-------------------\nA|R1|R2|R3|R4|R5|R6\nB|B1|B2|B3|B4|B5|B6\nC|C1|C2|C3|C4|C5|C6\nD|D1|D2|D3|D4|D5|D6\nE|E1|E2|E3|E4|E5|E6";
-    saveGame.bag = "A1,A2,A3,A4,A5,A6,A7";
-    saveGame.currentPlayer = "Duy";
-
-    // writeFile(saveGame);
-    SaveGame sg = readFile("savegame.txt");
-
-    // std::cout << sg.playerName1 << std::endl;
-    // std::cout << sg.playerScore1 << std::endl;
-    // std::cout << sg.playerHand1 << std::endl;
-
-    return 0;
 }
---
+++ A2_APT/code/IOCode.h
@@ -0,0 +1,35 @@
+#ifndef IOCODE_H
+#define IOCODE_H
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+
+ struct SaveGame
+    {
+        std::string playerName1;
+        int playerScore1;
+        std::string playerHand1;
+
+        std::string playerName2;
+        int playerScore2;
+        std::string playerHand2;
+
+        std::string board;
+        std::string bag;
+        std::string currentPlayer;
+    };
+
+class IOCode
+{
+public:
+    IOCode();
+    ~IOCode();
+
+    void writeFile(SaveGame s);
+    SaveGame readFile(std::string fileName);
+
+
+};
+
+#endif //IOCODE_H
